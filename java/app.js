0bb828290f7271e500c2fa378719ea9fcf0b09ed
dylancori <dylancoriglian@gmail.com>
2022-08-24 10:31:29 -0300

    .

---
+++ java/app.js
@@ -0,0 +1,14 @@
+//variables
+
+
+let curso = "coderhouse"
+console.log(curso);
+let numeroDeAlumnos = 181;
+console.log(numeroDeAlumnos);
+let enCurso = true;
+console.log(enCurso);
+
+//tipos de datos
+//numeros
+//cadenad de caracteres o STRINGS
+//buleanos o booleans TRUE - FALSE

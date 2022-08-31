c1f07e3dfdf28b558568ca7462ea2bd70edc6446
dylancori <dylancoriglian@gmail.com>
2022-08-31 11:10:07 -0300

    primer desafio

---
+++ js/desafio1.js
@@ -0,0 +1,8 @@
+
+for (let i = 1; i <= 20; i++) {
+    if (i <=10) {
+      console.log(`el invitado ${i} va a mesa 1`);
+    } else {
+      console.log(`el invitado ${i} va a mesa 2 `);
+    }
+  }

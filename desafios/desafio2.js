8e46f1d5706fc3afdf57a5b6e9f2df6b6c3f9ac6
dylancori <dylancoriglian@gmail.com>
2022-09-08 22:47:21 -0300

    a

--- desafios/desafio2.js
+++ desafios/desafio2.js
@@ -1,23 +1,54 @@
 let usuario = prompt("ingrese usuario (1 al 4)")
 const usuariolog = "1234"
-const usuariolog2 = "1122"
 const passlog = "123"
 
+class contactos{
+    constructor(id,nombre,alias,banco){
+        this.id = id
+        this.nombre =  nombre    
+        this.alias = alias
+        this.banco = banco
+    }
+}
+const listacontactos = [];
 
-if (usuario == usuariolog  ||  usuario == usuariolog2) {
+listacontactos.push(new contactos(1,"juan","juanmp","mercadopago"));
+listacontactos.push(new contactos(2,"elias","eliasfargan","msantander"));
+listacontactos.push(new contactos(3,"martin","martingali","galicia"));
+listacontactos.push(new contactos(4,"manu","manubrubank","brubank"));
+
+
+if (usuario == usuariolog ) {
     let password = prompt("ingrese la password(1 al 3)")
     if (password == passlog) {
         console.log("bienvenido usuario " + usuario)
         let operacion = Number(prompt(` que operacion desea realizar?"
          1-transsaccion 
-         2-hacer un pago`));
+         2-hacer un pago
+         3-ver billetera`));
 
         if (operacion == 1) {
             console.log('transaccion')
+            alert(` contactos disponibles
+            1 - juan
+            2 - elias
+            3 - martin
+            4 - manu
             
+            
+            `)
+            let contacto = Number(prompt(`seleccionar contacto`))
+            listacontactos.forEach((contacto) =>{
+                console.log(contacto.nombre);
+            })
+            
+
+
+
+
         }else if (operacion ==2) {
             console.log("hacer un pago")
-
+            let cuenta = Number(prompt('numero de pago'))
             let importe = Number(prompt('seleccione importe'));
             let cuotas = Number(prompt('seleccione cuotas'));
             function pago(importe, cuotas) {
@@ -25,19 +56,19 @@ if (usuario == usuariolog  ||  usuario == usuariolog2) {
                 return resultado
             }
             let resultado = pago(importe,cuotas);
-            console.log('el total a pagar es ' + resultado)
+            console.log(`el total a pagar en cuenta N-${cuenta} es $` + resultado )
 
             
             let tarjeta = prompt(`seleccione tarjeta: 
-            visa(10%of)
-            master(20%)`);
+            1-visa(10%of)
+            2-master(20%)`);
             function mediodepago (tarjeta) {
                 let x
                 switch(tarjeta){
-                    case 'visa':
+                    case '1':
                         x = resultado * 90/100;
                         break;
-                    case 'master':
+                    case '2':
                         x = resultado * 80/100;
                         break;
                     
@@ -45,13 +76,14 @@ if (usuario == usuariolog  ||  usuario == usuariolog2) {
              return x 
             }
             
-            console.log( 'con el descuento quedaria en ' + mediodepago(tarjeta))
+            console.log( 'con el descuento quedaria en $' + mediodepago(tarjeta))
         }
         
     } else{
         console.log("password incorrecta")
     }
 
-} else{
-    console.log("usuario no reconocido")
+} else {
+    alert('USUARIO NO RECONOCIDO')
+    // console.log("usuario no reconocido")
 }
---
+++ desafios/desafioo3.js
@@ -0,0 +1,11 @@
+class contactos{
+    constructor(nombre,alias,banco){
+        this.nombre =  nombre    
+        this.alias = alias
+        this.banco = banco
+    }
+}
+let contacto1 = new contactos("juan","juanmp","mercadopago")
+let contacto2 = new contactos("elias","eliasfargan","msantander")
+let contacto3 = new contactos("martin","martingali","galicia")
+let contacto4 = new contactos("manu","manubrubank","burbank")
